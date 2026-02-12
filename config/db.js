df3ae0404da0d7003047ca37c3f56cea45664051
Prashant Dhaigude <prashantdhaigude530@gmail.com>
2026-02-12 10:52:45 +0530

    DB Push

--- config/db.js
+++ config/db.js
@@ -4,33 +4,31 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config({
   path: process.env.DOCKER ? ".env.docker" : ".env.local",
 });
-
-// // ✅ Sequelize using ENV (NO hardcode)
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT || 3306,
-//     dialect: "mysql",
-//     logging: false, // production clean logs
-//   }
-// );
+ 
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    logging: false, // production clean logs
+  }
+);
 
 // module.exports = sequelize;
 
-const sequelize = new Sequelize(
-  "repaircrm_db",
-  "root",
-  "",
-  {
-    host: "localhost",
-    port: 3306,
-    dialect: 'mysql',
-    logging: false,
-  }
-);
+// const sequelize = new Sequelize(
+//    "repaircrm_db",
+//    "root",
+//   "",
+//   {
+//     host: "localhost",
+//     port: 3306,
+//     dialect: 'mysql',
+//   }
+// );
 
 
 module.exports = sequelize;
